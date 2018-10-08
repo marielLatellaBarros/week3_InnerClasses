054348cb821f0b8496d35411af866a8f67c5b5d5
Mariel Latella Barros <mariel.latella@gmail.com>
2018-10-08 09:48:18 +0200

    EXAMPLE of adding a gear making use of the PUBLIC inner class - NOT DONE

--- src/GearBoxApp.java
+++ src/GearBoxApp.java
@@ -2,6 +2,7 @@ public class GearBoxApp {
     public static void main (String[] args) {
 
         Gearbox fiatGearBox = new Gearbox(2);
-//        fiatGearBox.addGear(fiatGearBox.new Gear());
+        fiatGearBox.addGear(fiatGearBox.new Gear());
+        fiatGearBox.addGear(fiatGearBox.new Gear());
     }
 }
