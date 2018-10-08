3beef2a228a33e1059abfd32cbe8f9e583cb8648
Mariel Latella Barros <mariel.latella@gmail.com>
2018-10-08 09:28:44 +0200

    add an example of creating an object via a public inner class. This is feasible, but not done. The outer class should be the only interaction with the user, and not the inner class used by the developer. The inner class is used by the outer class.

---
+++ src/GearBoxApp.java
@@ -0,0 +1,7 @@
+public class GearBoxApp {
+    public static void main (String[] args) {
+
+        Gearbox fiatGearBox = new Gearbox(2);
+//        fiatGearBox.addGear(fiatGearBox.new Gear());
+    }
+}
